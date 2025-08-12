980dee6186e2c86547a08490f02b8aa72f7b9c4f
Abhi Dudhatra <dudhatraabhi@gmail.com>
2025-08-12 14:56:57 +0530

    commit

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return (
+        <h2>chai aur abhi </h2>
+    )
+}
+export default Chai
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+function App() {
+  
+
+  return (
+    <>
+    <Chai/>
+    <p>chai aur react</p>
+    <h2>hello abhi</h2>
+    </>
+    
+  )
+}
+
+export default App
