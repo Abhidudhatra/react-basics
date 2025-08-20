986ba357c11a9abeadeee40f76256ff5a572df79
Abhi Dudhatra <dudhatraabhi@gmail.com>
2025-08-20 14:24:23 +0530

    counter

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,13 @@
 import Chai from "./chai"
 function App() {
+
+  const username ="abhidudhatra92"
   
 
   return (
     <>
     <Chai/>
-    <p>chai aur react</p>
+    <p>chai aur react {username}</p>
     <h2>hello abhi</h2>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,43 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp(){
+  return(
+    <div>
+      <h1>custom app!</h1>
+    </div>
+  )
+}
+
+// const ReactElement={
+//     type:'a',
+//     props:{
+//         href:"https://www.google.com/",
+//         target:'_blank',
+        
+//     },
+//     children:'click me to visit google'
+
+// }
+
+const Anotherelement=(
+  <a href="https://www.google.com" target='_blank'>visit google</a>
+
+)
+const anotheruser="chai aur react"
+
+const reactElement= React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'click me to visit google',
+
+  Anotherelement
+
+
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+  reactElement
 )
